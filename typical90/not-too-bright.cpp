3583ca81d5f23cb9a264ce35002f6843557bd2c5
tetsukamen <tetsukamen00@gmail.com>
2022-05-30 07:05:43 +0900

    not-too-bright

---
+++ typical90/not-too-bright.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int H, W;
+    cin >> H >> W;
+
+    if (H == 1 || W == 1)
+    {
+        cout << H * W << endl;
+    }
+    else
+    {
+        cout << ((H + 1) / 2) * ((W + 1) / 2) << endl;
+    }
+}
