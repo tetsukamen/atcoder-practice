7238cb366990fa877ed91d029abd60e562296b8a
tetsuyawada <tetsukamen00@gmail.com>
2022-06-18 23:11:24 +0900

    minimum-coins

---
+++ typical90/minimum-coins.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cmath>
+#include <queue>
+#include <deque>
+#include <map>
+using namespace std;
+
+int main()
+{
+    long long N, A, B, C;
+    cin >> N >> A >> B >> C;
+
+    int answer = 9999;
+    int L = 9999;
+    int X, Y, Z;
+    for (int x = 0; x <= L; x++)
+    {
+        for (int y = 0; x + y <= L; y++)
+        {
+            if ((A * x + B * y) <= N && (N - (A * x + B * y)) % C == 0)
+            {
+                int z = (N - (A * x + B * y)) / C;
+                answer = min(answer, x + y + z);
+            }
+        }
+    }
+    cout << answer << endl;
+}
